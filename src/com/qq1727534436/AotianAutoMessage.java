c6d500354986235867b1ebf38e2bd8e24a4c4993
hubsab <1727534436@qq.com>
2017-07-16 11:33:23 +0800

    Update AotianAutoMessage.java

--- src/com/qq1727534436/AotianAutoMessage.java
+++ src/com/qq1727534436/AotianAutoMessage.java
@@ -3,49 +3,26 @@ package com.qq1727534436;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class AotianAutoMessage extends JavaPlugin{
-	private ArrayList<String> messages=new ArrayList<String>();
-	private int time=60;
+	private ArrayList<String> message=new ArrayList<String>();
 	private int index=0;
 	@Override
 	public void onEnable() {
-		//Read
-		if(!getDataFolder().exists()) {
-			getDataFolder().mkdir();
-		}
-		if(new File(getDataFolder(),"config.yml").exists()) {
-			messages=(ArrayList<String>) getConfig().getStringList("Message");
-			new BukkitRunnable() {
-				@SuppressWarnings("deprecation")
-				@Override
-				public void run() {
-					if(messages.size()==0) {
-						getLogger().log(Level.WARNING,"Please set the message!");
-						cancel();
-						getServer().getPluginManager().disablePlugin(getServer().getPluginManager().getPlugin("AotianAutoMessage"));
-					}
-					for(int i=0;i<getServer().getOnlinePlayers().length;i++) {
-						getServer().getOnlinePlayers()[i].sendMessage(messages.get(index));
-					}
-					index++;
-					if(index==messages.size()) {
-						index=0;
-					}
-				}
-			}.runTaskTimer(this,0,time*20);
+		if(getDataFolder().exists()) {
+			message=(ArrayList<String>) getConfig().getStringList("Message");
+			a();
 		}else {
 			try {
+				getDataFolder().mkdir();
 				new File(getDataFolder(),"config.yml").createNewFile();
-				messages.add("Test");
-				getConfig().set("Message",messages);
-				getConfig().set("Time", 60);
+				message.add("Test");
+				getConfig().set("Message",message);
 				saveConfig();
-				getServer().getPluginManager().disablePlugin(this);
+				a();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -54,8 +31,22 @@ public class AotianAutoMessage extends JavaPlugin{
 	}
 	@Override
 	public void onDisable() {
-		//Save
-		getConfig().set("Message",messages);
-		getLogger().info("AotianAutoMessage disable!Autuor:youkucola(QQ:1727534436)!");
+		getLogger().info("AotianAutuoMessage is disable!Autuor:youkucola(QQ:1727534436)!");
+	}
+	public void a() {
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				if(message.size()!=0) {
+					getServer().broadcastMessage(message.get(index));
+					if(++index==message.size()) {
+						index=0;
+					}
+				}else {
+					message.add("Test");
+					throw new NullPointerException("Wring!Message is not set!Use default Message!");
+				}
+			}
+		}.runTaskTimer(this, 0, 20*30);
 	}
 }
